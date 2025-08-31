db50cef8918f2f52277a44b472ba53eed890cd50
hamed-esk <53735642+hamed-esk@users.noreply.github.com>
2025-08-31 17:26:39 +0900

    Update introduction

--- src/web/main_page.py
+++ src/web/main_page.py
@@ -1,18 +1,9 @@
 import streamlit as st
 import os
-from src.word import JPWord
 
+st.title("Japanese Video Dictionary")
+st.markdown("Welcome to the Japanese Video Dictionary! Use the sidebar to navigate through different sections.")
+# My youtube channel
+st.markdown("Check out my [YouTube channel](https://www.youtube.com/@JapaneseVideoDictionary) for more Japanese learning content!")
+st.markdown("Feel free to explore the resources and improve your Japanese skills!")
 
-def fetch_and_show_word():
-    json_files = [f[:-5] for f in os.listdir("Word") if f.endswith(".json")]
-    json_files.sort()
-    if st.query_params["word"] in json_files:
-        with open("Word/" + st.query_params["word"] + ".json", "r", encoding="utf-8") as file:
-            w = JPWord.model_validate_json(file.read())
-        w.show_in_streamlit(st)
-
-
-if "word" in st.query_params:
-    fetch_and_show_word()
-else:
-    st.title("Japanese Video Dictionary")
