fc7a8ab92656617c1e7a8d2aba07fe9172d561d7
hamed-esk <53735642+hamed-esk@users.noreply.github.com>
2025-09-07 20:40:15 +0900

    check functional import

--- src/web/v.py
+++ src/web/v.py
@@ -2,7 +2,13 @@ import streamlit as st
 
 from src.word.JPWord import JPWord
 
-from ..db import db_word  # from src.db import db_word
+from src.db.db_word import (
+    check_user_word_card,
+    add_user_word_card,
+    remove_user_word_card,
+    get_user_word_cards,
+    get_words,
+)
 
 auth = st.session_state.get("auth", None)
 
@@ -19,11 +25,11 @@ if "w" in st.query_params:
         if st.button(":arrow_left: Back to Vocabularies"):
             st.query_params.pop("w")
             st.rerun()
-        card_added = db_word.check_user_word_card(auth, st.query_params["w"]) if auth else False
+        card_added = check_user_word_card(auth, st.query_params["w"]) if auth else False
         if card_added:
             if st.button(":star: remove from My Words"):
                 if auth:
-                    result = db_word.remove_user_word_card(auth, st.query_params["w"])
+                    result = remove_user_word_card(auth, st.query_params["w"])
                     st.toast(result[1], icon="✅" if result[0] else "❌")
                     st.rerun()
                 else:
@@ -31,20 +37,20 @@ if "w" in st.query_params:
         else:
             if st.button(":star: Add to My Words", type="primary"):
                 if auth:
-                    result = db_word.add_user_word_card(auth, st.query_params["w"])
+                    result = add_user_word_card(auth, st.query_params["w"])
                     st.toast(result[1], icon="✅" if result[0] else "❌")
                     st.rerun()
                 else:
                     st.toast("You need to be logged in to add words to your list.", icon="❌")
     fetch_and_show_word()
 else:
-    user_word_cards = db_word.get_user_word_cards(auth) if auth else []
+    user_word_cards = get_user_word_cards(auth) if auth else []
     marked_words = [w.get("key") for w in user_word_cards] if user_word_cards else []
     st.markdown("# JLPT Vocabularies")
 
     with st.expander("N4 Vocabularies"):
         with st.container(horizontal=True):
-            for w in db_word.get_words(4):
+            for w in get_words(4):
                 word = str(w.get("word"))
                 if st.button(word, type="primary" if word in marked_words else "secondary"):
                     st.query_params.update({"w": word})
@@ -52,7 +58,7 @@ else:
 
     with st.expander("N3 Vocabularies"):
         with st.container(horizontal=True):
-            for w in db_word.get_words(3):
+            for w in get_words(3):
                 word = str(w.get("word", ""))
                 if st.button(word, type="primary" if word in marked_words else "secondary"):
                     st.query_params.update({"w": word})
@@ -60,7 +66,7 @@ else:
 
     with st.expander("N2 Vocabularies"):
         with st.container(horizontal=True):
-            for w in db_word.get_words(2):
+            for w in get_words(2):
                 word = str(w.get("word", ""))
                 if st.button(word, type="primary" if word in marked_words else "secondary"):
                     st.query_params.update({"w": word})
